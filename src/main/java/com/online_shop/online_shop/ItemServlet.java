7ec4899d21ec3d19aa03f92b5f6a047ba5b8a838
Danylo <danilnovikov759@gmail.com>
2021-02-28 19:23:18 +0200

    - Move all items-related stuff to dedicated subfolder. Add page template for item admin page.

--- src/main/java/com/online_shop/online_shop/ItemServlet.java
+++ src/main/java/com/online_shop/online_shop/ItemServlet.java
@@ -36,7 +36,7 @@ public class ItemServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher view = req.getRequestDispatcher("item.jsp");
+        RequestDispatcher view = req.getRequestDispatcher("item/item.jsp");
 
         itemId = Integer.parseInt(req.getParameter("id"));
         reviewScore = -1;
